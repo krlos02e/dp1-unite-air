e7d473dbb35f013917bd7f37f1da4d7ffee53fd8
Carlos Heredia <cherediar123@gmail.com>
2026-06-24 22:08:09 -0500

    de vuelta el cors bien

--- dp1-backend/src/main/java/pe/edu/pucp/uniteair/dp1backend/config/SecurityConfig.java
+++ dp1-backend/src/main/java/pe/edu/pucp/uniteair/dp1backend/config/SecurityConfig.java
@@ -69,11 +69,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of(
-                "http://localhost:*",
-                "http://127.0.0.1:*",
-                "http://0.0.0.0:*"
-        ));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
